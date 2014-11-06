d533aa1c6e8c1cb1a3de77b260814b48410705c2
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-11-06 15:31:59 +0300

    fix cloud-init
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -7,6 +7,8 @@ systemctl enable cloud-config
 systemctl enable cloud-final
 systemctl enable cloud-init-local
 systemctl enable cloud-init
+
+sed -i '/.*SmartOS.*/d' /usr/lib/python2.7/site-packages/cloudinit/settings.py
 sync
 fstrim  -v /
 nohup systemctl stop wicked.service &
