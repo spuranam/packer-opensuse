560168cf71458b2455b74b89a7dc1b79cd89d6d5
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-04-07 18:37:15 +0300

    fix opensuse
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,13 +2,6 @@
 
 rm -rf /tmp/*
 
-zypper install -y cloud-init
-systemctl enable cloud-config
-systemctl enable cloud-final
-systemctl enable cloud-init-local
-systemctl enable cloud-init
-
-sed -i '/.*SmartOS.*/d' /usr/lib/python2.7/site-packages/cloudinit/settings.py
 sync
 fstrim  -v /
 nohup systemctl stop wicked.service &
