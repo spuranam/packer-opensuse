365e3a9ea8110af868ef94c700328c24643250c0
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-12-11 18:41:37 +0000

    fix leap
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -ex
 
-rm -f /etc/zypp/repos.d/openSUSE-13.2-0.repo /etc/zypp/repos.d/http-mirror*
-
-rpm --import http://download.opensuse.org/distribution/13.2/repo/oss/gpg-pubkey-3dbdc284-53674dd4.asc
-rpm --import http://download.opensuse.org/distribution/13.2/repo/oss/gpg-pubkey-307e3d54-4be01a65.asc
-zypper ar http://download.opensuse.org/distribution/13.2/repo/oss/ opensuse-13.2-oss
-zypper ar http://download.opensuse.org/distribution/13.2/repo/non-oss/ opensuse-13.2-non-oss
-zypper ar -t rpm-md -c -n opensuse-13.2-update -g -f http://download.opensuse.org/update/13.2/ opensuse-13.2-update
-zypper ar -t rpm-md -c -n opensuse-13.2-update-non-oss  -g -f http://download.opensuse.org/update/13.2-non-oss/ opensuse-13.2-update-non-oss
 zypper refresh
-zypper update -y --auto-agree-with-licenses --force-resolution
+zypper --non-interactive update --auto-agree-with-licenses --force-resolution
